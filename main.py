4a44ccc20a0934ace26acd7d5f5f34ae1eb85763
yagoelon <116230533+yagoelon@users.noreply.github.com>
2022-10-20 00:54:17 +0000

    Aprendi a exibir mais variáveis na tela

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 # Meu primeiro projeto Python!!
+
 #print() = comando de saida
 print("Hello World")
 
+#Quando quiser guardar uma String (Frase)
+nome = "Yago Lopes"
 
+#Quando quiser gaurdar um número inteiro
+idade = 22
+
+#Exibir o meu nome (que está dentro da variável nome)
+print(nome)
+print(idade)
 
 '''
 Comentário em bloco
