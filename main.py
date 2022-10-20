7fc4ad6a41abce7cfcf9363b570cd76154ebd859
yagoelon <116230533+yagoelon@users.noreply.github.com>
2022-10-20 01:18:39 +0000

    Aprendendo ++

--- main.py
+++ main.py
@@ -13,6 +13,19 @@ idade = 22
 print(nome)
 print(idade)
 
+#Para exibir uma frase + o conteúdo que queira
+print("Meu nome é " + nome+"\n")
+print("Minha idade é "+str(idade)+" anos\n")
+print(f"Minha idade é {idade} anos\n")
+print("Minha idade é {} anos".format(idade))
+
+#Para exibir uma frase + o conteúdo que queira usando o "format"
+print("Meu nome é {} e tenho {} anos".format(nome,idade))
+
+#Para exibir uma frase + o conteúdo que queira usando o "f"
+print(f"Meu nome é {nome} e tenho {idade} anos")
+
+
 '''
 Comentário em bloco
 '''
